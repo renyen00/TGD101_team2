c2c94e4dd536dbdf07372006859147dd58efa43b
Owen176Kao <a3899383@gmail.com>
2022-06-28 14:35:23 +0800

    6/28

---
+++ src/php/reportSubmit.php
@@ -0,0 +1,42 @@
+<?php
+
+    // echo $_GET['reasons'];
+    $reason_id = $_GET['reasons'];
+
+    
+
+
+    include("Connection_server.php");
+
+    //---------------------------------------------------
+
+    //建立SQL語法
+    $sql = "INSERT INTO REPORT (TIME, REASON, STATE) VALUES (now(), ?, '未審核');";
+
+    //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+    $statement = $pdo->prepare($sql);
+    $statement->bindParam(1, $_GET['reasons']);
+    $statement->execute();
+
+    //抓出全部且依照順序封裝成一個二維陣列
+    // $data = $statement->fetchAll();
+
+    // print_r($data);
+
+    // $process_data = [];
+    // //將二維陣列取出顯示其值
+    // foreach($data as $index => $row){
+    //         $temp = [];
+
+    //         for($i=0; $i<(count($row)/2); $i++){
+                    
+    //                 array_push($temp, $row[$i]);
+    //         }
+
+    //         array_push($process_data, $temp);
+    // }
+
+    // // print_r($process_data[0]);
+    // echo json_encode($process_data);
+    header("Location:../reportForm.html");
+?>

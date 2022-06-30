6eef22fdc573b609696e391a744b10c25329e1b0
renyen00 <renyen00@gmail.com>
2022-07-01 00:55:41 +0800

    backend update

---
+++ src/php/backend1_4.php
@@ -0,0 +1,34 @@
+<?php
+
+    include('Connection_self.php');
+    //---------------------------------------------------
+
+
+    //建立SQL語法
+    $sql = "SELECT * FROM BOT;";
+
+    //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+    $statement = $pdo->prepare($sql);
+    $statement ->execute();
+    //抓出全部且依照順序封裝成一個二維陣列
+    $data = $statement->fetchAll();
+
+//     print_r($data);
+
+    $process_data = [];
+    //將二維陣列取出顯示其值
+    foreach($data as $index => $row){
+            $temp = [];
+
+            for($i=0; $i<(count($row)/2); $i++){
+                    
+                    array_push($temp, $row[$i]);
+            }
+
+            array_push($process_data, $temp);
+    }
+
+    // print_r($process_data[0]);
+    echo json_encode($process_data);
+
+?>

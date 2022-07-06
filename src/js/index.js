8a0e27676ce51c3c8705b258d91e6f582a661dd2
Owen176Kao <a3899383@gmail.com>
2022-07-06 18:11:56 +0800

    deleted 5 birds on index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,136 @@
-// gsap.registerPlugin(ScrollTrigger);
-// var d = gsap.timeline({
-//     defaults: {
-//         ease: "none",
-//         scrollTrigger: {
-//             trigger: ".triggerIsland",     
-//             start: "top center", 
-//             end: "+= 4000",
-//             scrub:"true",  
-//             markers: true,  
-//             // pin: "#islandWrapper",
-//         }       
-//     }
-//     }
-//   );
-
+gsap.registerPlugin(ScrollTrigger);
+var d = gsap.timeline({
+    defaults: {
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".triggerIsland",     
+            start: "top center", 
+            end: "+= 4000",
+            scrub:"true",  
+            markers: true,  
+            // pin: "#islandWrapper",
+        }       
+    }
+    }
+  );
 // 雲
-// d.to(".index_cloud_1", {x: -1200})
-// .to(".index_cloud_2", {x: -1200})
-// .to(".index_cloud_3", {x: -1200})
-// .to(".index_cloud_4", {x: -1200})
-// .to(".index_cloud_5", {x: -1200})
-// .to(".index_cloud_6", {x: -1200})
-// .to(".index_cloud_7", {x: -1200})
-// .to(".islandAnimationTest", {x: 500});
+gsap.to(".index_cloud_1", {           
+    x:-1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+
+gsap.to(".index_cloud_2" , {           
+    x:-1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+gsap.to(".index_cloud_3" , {           
+    x:1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+gsap.to(".index_cloud_4" , {           
+    x:1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+gsap.to(".index_cloud_5" , {           
+    x:1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0 ,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+gsap.to(".index_cloud_6" , {           
+    x:1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+
+gsap.to(".index_cloud_7" , {           
+    x:-1000,
+    duration: 5,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".island",     
+        start: "top top", 
+        end:"+=1500",   
+        scrub: !0,    
+        // markers: true,
+        pin: ".island",
+    }       
+})
+
+//鳥
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
+
+gsap.to(".index_bird",{          
+    duration:8,
+    scrollTrigger: {
+        trigger:"#motionPath",
+        start:"top top",
+        // end: "+=6000",
+        scrub: true,           
+        // markers: true,
+        pin: "#motionPath",        
+    },
+    motionPath: {
+    path: "#motionPath",
+    align: "#motionPath",
+    ease: "none",
+    alignOrigin: [0.5, 0.5],
+    autoRotate: true, 
+    start: 0,
+    end: 1,
+  }
+});
 
 var controller = new ScrollMagic.Controller();
 var stickyMap = new TimelineMax();
@@ -79,26 +187,3 @@ var stickyMap = new TimelineMax();
         duration: '1000%',
     }).setPin('#islandWrapper').setTween(stickyMap).addTo(controller);
     // .addIndicators() 
-
-
-//鳥
-// gsap.to(".index_bird",{          
-//     duration:8,
-//     scrollTrigger: {
-//         trigger:"#motionPath",
-//         start:"top top",
-//         // end: "+=6000",
-//         scrub: true,           
-//         // markers: true,
-//         pin: "#motionPath",        
-//     },
-//     motionPath: {
-//     path: "#motionPath",
-//     align: "#motionPath",
-//     ease: "none",
-//     alignOrigin: [0.5, 0.5],
-//     autoRotate: true, 
-//     start: 0,
-//     end: 1,
-//   }
-// });

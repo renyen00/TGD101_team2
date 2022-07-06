6182009f7ede84882c7448d2746c40841cb606b7
renyen00 <renyen00@gmail.com>
2022-07-07 00:21:42 +0800

    7/7

--- src/php/backend3_insert.php
+++ src/php/backend3_insert.php
@@ -94,54 +94,6 @@
         $statement ->bindValue(':pic3',$pic3);
         $statement ->bindValue(':id',$id);
         $statement ->execute();
-    }else if($_GET['quest'] == 4){
-        $n=$_GET['n'];
-
-        $sql = "DELETE FROM BOT WHERE ID = :n;
-        ";
-    
-        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-        $statement = $pdo->prepare($sql);
-        $statement ->bindValue(':n',$n);
-        $statement ->execute();
-    }else if($_GET['quest'] == 5){
-        $n=$_GET['n'];
-
-        $sql = "DELETE FROM MESSAGE WHERE ID = :n;
-        ";
-    
-        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-        $statement = $pdo->prepare($sql);
-        $statement ->bindValue(':n',$n);
-        $statement ->execute();
-    }else if($_GET['quest'] == 6){
-        $content=$_GET['content'];
-        $n=$_GET['id'];
-
-        $sql = "UPDATE SERVICE set REPLY_STATUS = 'yes',REPLY = :content where ID = :n
-        ";
-    
-        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-        $statement = $pdo->prepare($sql);
-        $statement ->bindValue(':content',$content);
-        $statement ->bindValue(':n',$n);
-        $statement ->execute();
-    }else if($_GET['quest'] == 7){
-        $s=$_GET['s'];
-        $id=$_GET['id'];
-
-        $sql = "UPDATE REPORT set STATE = :s where ID = :id
-        ";
-    
-        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-        $statement = $pdo->prepare($sql);
-        $statement ->bindValue(':s',$s);
-        $statement ->bindValue(':id',$id);
-        $statement ->execute();
     }
-
-
-
-
 
 ?>
